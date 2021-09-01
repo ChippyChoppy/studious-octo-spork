e4b29cbeebb9941cb5bddb0b24bfd7fdd4afe8e4
ChippyChoppy <rebeccamargaret13@gmail.com>
2021-08-31 22:20:43 -0400

    displaying

---
+++ src/index.js
@@ -0,0 +1,35 @@
+/*DOM Elements */
+const customerName = document.querySelector("#customer-name")
+const customerTotal = document.querySelector("#customer-total")
+
+/* Render Functions */
+const renderCustomerTotal = orderObj => {
+    console.log(orderObj)
+    let totalPurchases = 0
+    totalPurchases += orderObj.total
+    const li = document.createElement("li")
+    li.textContent = `${totalPurchases}`
+    customerTotal.append(li)
+}
+
+const renderTop10Customers = customerObj => {
+        console.log(customerObj)
+
+    customerName.textContent = customerObj.name
+    customerObj.orders.forEach(renderCustomerTotal)
+}
+
+
+
+/* Fetch Functions */
+
+const getOneCustomer = id => {
+    fetch(`http://localhost:3000/api/v1/customers/${id}`)
+        .then(r => r.json())
+        .then(customerObj => {
+            renderTop10Customers(customerObj)
+        })
+}
+
+/* Initialize */
+getOneCustomer(69)
